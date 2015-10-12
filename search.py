79fa87fe93360ecf00a4f2e8dd33002c19357c84
Rob Moore <rmoore.pageonepower@gmail.com>
2015-10-12 13:58:12 -0600

    added dropdown support

--- search.py
+++ search.py
@@ -1,44 +1,47 @@
 from tkinter import *
+from tkinter import ttk
 import webbrowser
 
 engine_dict = {"google":"https://www.google.com/search?q=",
                 "bing":"https://www.bing.com/search?q=",
                 "yahoo":"https://search.yahoo.com/search?p="}
 
-def select_engine():
-    prompt = "Select a search engine. (Google, Bing, Yahoo)"
-    print(prompt)
-    while True:
-        search_engine = input('> ').lower()
-        if search_engine in engine_dict:
-            engine_name = search_engine.capitalize()
-            base_url = engine_dict[search_engine]
-        else:
-            print('I couldn\'t find that search engine. Try again.\n')
-            print(prompt)
-            continue
-        return engine_name, base_url
-
-engine_name, base_url = select_engine()
-
-root = Tk()
-root.title('Search in ' + engine_name)
-
-
-String_Entry = Entry(root)
-String_Entry.grid(row=0, column=0, padx=50, pady=50)
+search_engines = ['Google', 'Yahoo', 'Bing']
 
 
 def search():
-    url = base_url + str(String_Entry.get())
-    webbrowser.open(url)
+    if combo.get() == search_engines[0]:
+        base_url = engine_dict["google"]
+        url = base_url + str(String_Entry.get())
+        webbrowser.open(url)
+    elif combo.get() == search_engines[1]:
+        base_url = engine_dict["yahoo"]
+        url = base_url + str(String_Entry.get())
+        webbrowser.open(url)
+    elif combo.get() == search_engines[2]:
+        base_url = engine_dict["bing"]
+        url = base_url + str(String_Entry.get())
+        webbrowser.open(url)
+
+
+root = Tk()
+root.title('Search in ')
+
+
+
+String_Entry = Entry(root)
+String_Entry.grid(row=0, column=0, padx=10, pady=50)
 
 Search_Button = Button(root, text='Search', command=search)
 Search_Button.grid(row=0, column=1)
 
-
-
 Quit_Button = Button(root, text='Quit', command=quit)
-Quit_Button.grid(row=0, column=2)
+Quit_Button.grid(row=0, column=2, padx=10)
 
+combotest = StringVar()
+combo = ttk.Combobox(root, textvariable=combotest)
+combo['values'] = search_engines
+combo.grid(row=1, column=1)
+
+root.configure(background='grey')
 root.mainloop()
